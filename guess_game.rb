1215069d9ad3bf18bf4b09879dfc343d1fc45247
Alfonso Antonio <j.alfonso.antonio@gmail.com>
2017-08-21 16:48:55 -0400

    added 2 methods and alert for repeat guesses

--- guess_game.rb
+++ guess_game.rb
@@ -1,26 +1,44 @@
 puts "Welcome to the Guessing Game!"
 secretNum = rand(1..100)
-# print secretNum
+# alternate way to get random number
+# secretNum = (1..100).to_a.sample
+i = 0
+guess = 0
+guessArr = []
+
+def get_user_input
+  gets.chomp.to_i
+end
+
+def check_repeat arr, num
+  if arr.include? num
+    puts "already guessed this number, wasted guess :("
+  end
+end
+
 puts "Pick any Number between 1-100"
-guess = gets.chomp.to_i
+guess = get_user_input
+guessArr.push(guess)
 
-$i = 0
-
-while $i < 4
+while i < 4
   if guess < secretNum
-    $i += 1
+    i += 1
     puts "guess higher!"
-    guess = gets.chomp.to_i
+    guess = get_user_input
+    check_repeat guessArr, guess
+    guessArr.push guess
   elsif guess > secretNum
-    $i += 1
+    i += 1
     puts "guess lower!"
-    guess = gets.chomp.to_i
+    guess = get_user_input
+    check_repeat guessArr, guess
+    guessArr.push guess
   else
     break
   end
 end
 
-if $i >=4 && guess != secretNum
+if i >=4 && guess != secretNum
   puts "You lose :("
 else
   puts "you got it!"
